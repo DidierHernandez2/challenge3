52804164ed64d34920b4d291e4e44b7927f18de3
darhf <didier.hernandez1972@gmail.com>
2025-04-23 20:55:35 -0600

    Prueba de avanzar angular

--- src/control_trayectoria/control_trayectoria/pid_control.py
+++ src/control_trayectoria/control_trayectoria/pid_control.py
@@ -2,24 +2,25 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
-class ForwardPublisher(Node):
+class AngularPublisher(Node):
     def __init__(self):
-        super().__init__('forward_publisher')
+        super().__init__('angular_publisher')
+        # Publicador para cmd_vel
         self.cmd_pub = self.create_publisher(Twist, 'cmd_vel', 10)
-        # Publica comando cada 0.1s -> 10Hz
-        self.create_timer(0.1, self.publish_forward)
-        self.get_logger().info('ForwardPublisher iniciado: publicando velocidad constante hacia adelante.')
+        # Publica comando cada 0.1s (10Hz)
+        self.create_timer(0.1, self.publish_angular)
+        self.get_logger().info('AngularPublisher iniciado: publicando velocidad angular constante.')
 
-    def publish_forward(self):
+    def publish_angular(self):
         msg = Twist()
-        msg.linear.x = 0.2  # velocidad constante hacia adelante
-        msg.angular.z = 0.0
+        msg.linear.x = 0.0    # sin componente lineal
+        msg.angular.z = 0.5   # velocidad angular constante (rad/s)
         self.cmd_pub.publish(msg)
 
 
 def main():
     rclpy.init()
-    node = ForwardPublisher()
+    node = AngularPublisher()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
